74678169ad0966e2366f331ce2e1ad178e770a91
automaton641 <automaton641@gmail.com>
2020-04-20 04:21:04 -0500

    update

--- src/automaton641/dev/Main.java
+++ src/automaton641/dev/Main.java
@@ -5,7 +5,8 @@ import automaton641.lib.LibWindow;
 
 public class Main {
     public static void main(String[] args) {
-        LibWindow window = new LibWindow("Screen");
+        LibWindow window = new LibWindow("Dev");
+        window.addTextComponent(new String[]{"Welcome to DEV the game!", "click here to continue..."});
         window.show();
     }
 }
