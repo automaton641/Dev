9099e12832942da5980cc7c4c6db6045ff968002
automaton641 <automaton641@gmail.com>
2020-04-20 01:08:11 -0500

    update

---
+++ src/automaton641/dev/Main.java
@@ -0,0 +1,11 @@
+package automaton641.dev;
+
+import automaton641.lib.LibTerminal;
+import automaton641.lib.LibWindow;
+
+public class Main {
+    public static void main(String[] args) {
+        LibWindow window = new LibWindow("Screen");
+        window.show();
+    }
+}
